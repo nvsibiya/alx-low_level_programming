488b6598cf07cdd8a3210ad2ba66bc4a1fc1ca70
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2023-09-08 15:02:20 -0700

    Write a program that prints all possible combinations of two two-digit numbers.
    The numbers should range from 0 to 99
    The two numbers should be separated by a space
    All numbers should be printed with two digits. 1 should be printed as 01
    The combination of numbers must be separated by comma, followed by a space
    The combinations of numbers should be printed in ascending order
    00 01 and 01 00 are considered as the same combination of the numbers 0 and 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,24 @@
 #include <stdio.h>
 
-/**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
- */
+int main(void) {
+    int i, j;
 
-int main(void)
-{
-	int tens;
-	int ones;
-	int t;
-	int o;
+    for (i = 0; i <= 99; i++) {
+        for (j = i; j <= 99; j++) {
+            putchar((i / 10) + '0'); /* Print tens digit of i */
+            putchar((i % 10) + '0'); /* Print ones digit of i */
+            putchar(' '); /* Print a space */
+            putchar((j / 10) + '0'); /* Print tens digit of j */
+            putchar((j % 10) + '0'); /* Print ones digit of j */
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-			for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
+            if (i != 99 || j != 99) {
+                putchar(','); /* Print a comma */
+                putchar(' '); /* Print a space */
+            }
+        }
+    }
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
+    putchar('\n'); /* Print a newline */
 
-	return (0);
+    return 0;
 }
