60915ef28a502a33a86882d697cd3fe1e3bdb916
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2023-09-29 16:06:52 -0700

    Write a program that multiplies two numbers.
    Your program should print the result of the multiplication, followed by a new line
    You can assume that the two numbers and result of the multiplication can be stored in an integer
    If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Multiplies two numbers
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: 0 on success, 1 on error
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(int argc, char *argv[]) {
-    int num1, num2, result;
-
-   
-    if (argc != 3) {
-        printf("Error\n");
-        return 1;  
-    }
-
-    
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-
-    result = num1 * num2;
-
-    printf("%d\n", result);
-
-    return (0);
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
