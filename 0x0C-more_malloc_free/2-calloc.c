6084378d16c8ecd02c4d8c5194586e153b9d7e88
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2023-10-05 16:36:04 -0700

    fixed betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,20 +9,18 @@
  * Return: A pointer to the allocated and initialized memory
  *         or NULL if nmemb or size is 0 or if malloc fails
  */
-void *_calloc(unsigned int nmemb, unsigned int size) {
-    void *ptr;
-    unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *point;
+	unsigned int i;
 
-    ptr = malloc(nmemb * size);
-
-    if (ptr == NULL)
-        return (NULL);
-
-    for (i = 0; i < nmemb * size; i++)
-        *((char *)ptr + i) = 0;
-
-    return (ptr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	point = malloc(nmemb * size);
+	if (point == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)point + i) = 0;
+	return (point);
 }
